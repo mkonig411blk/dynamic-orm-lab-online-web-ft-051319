96da633377ed4ba097b639fc3ea59065a2acf965
Margot Konig <margot.konig@blackrock.com>
2019-07-23 16:38:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,8 +50,8 @@ def save
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '?'"
-    DB[:conn].execute(sql, name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
   end
 
   def self.find_by(attribute_hash)
